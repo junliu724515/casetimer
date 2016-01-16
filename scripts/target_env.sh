ebf134999642fd07c1ce35f0a4a9f78cddc4f211
Jun Liu <jun.liu@salesforce.com>
2016-01-16 22:23:50 +1100

    removed target org credentials

--- scripts/target_env.sh
+++ scripts/target_env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Enter your target org crendentials to deploy here
 #
-export sfdc_username=arangwala@dealertrack.demo
-export sfdc_pass_and_token=salesforce1
+export sfdc_username=
+export sfdc_pass_and_token=
 export sfdc_server_url=https://login.salesforce.com
